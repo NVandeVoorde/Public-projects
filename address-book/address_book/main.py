038b1b69059ff44d76c8bb42bdf9c724764040db
NVandeVoorde <nicolasvdvoorde@gmail.com>
2024-01-07 14:08:41 +0100

    cleanup + ignore

--- address-book/address_book/main.py
+++ address-book/address_book/main.py
@@ -1,19 +1,21 @@
 import tkinter as tk 
 
 root = tk.Tk()
+root.geometry("800x800")
+root.title("Address book app")
 
 # INTRO
 frm_intro = tk.Frame(root)
 
 lbl_intro = tk.Label(frm_intro, text="CRUD Application for address book")
-lbl_intro.grid(0,0)
+lbl_intro.pack()
 
-frm_intro.grid(0,0)
+frm_intro.pack()
 
 # BUTTONS
 frm_buttons = tk.Frame(root)
-
-frm_buttons.grid(1, 0)
+frm_buttons.columnconfigure(0, weight = 1)
+frm_buttons.columnconfigure(1, weight = 1)
 
 btn_add = tk.Button(
     frm_buttons, 
@@ -33,9 +35,10 @@ btn_delete = tk.Button(
     fg = 'white',
 )
 
-btn_add.grid(0, 0)
-btn_delete.grid(0, 1)
+btn_add.grid(row = 0, column = 0)
+btn_delete.grid(row = 0, column = 1)
 
+frm_buttons.pack(fill= 'x')
 
 # TABLE
 

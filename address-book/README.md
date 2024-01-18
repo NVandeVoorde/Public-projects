7d63f9362e61e9dae7aa9017f99db8520632ff8e
NVandeVoorde <nicolasvdvoorde@gmail.com>
2024-01-18 21:17:49 +0100

    seperate logic and UI + fill license and readme

--- address-book/README.md
+++ address-book/README.md
@@ -1,5 +1,34 @@
 
-A description of the project and its purpose
-Installation instructions
-Dependencies and requirements
-Usage instruction
+
+# General purpose
+
+This is a small personal project to setup a lightweight database locally and build a basic UI to add, update and delete records in said database. 
+The application allows users to 
+The idea was to research as much as possible on my own, without following any kind of tutorial.
+Any feedback is welcome, but remember that these are my first steps in python :)!
+
+# The address book app
+
+## Use 
+
+Users can download the script and run the main.py file to open the GUI. 
+The local database contains a few example rows. 
+The GUI allows them to add addresses to the database. 
+The script does contain a module that shows the initialization of the database. 
+
+[!IMPORTANT]
+The database uses an open source website to retrieve the zipcodes of Belgian municipalities, of wich only the Flemish ones were selected. 
+Users can therefore only select municipalities in the Flemish region in Belgium.
+
+## Goals 
+
+I had several goals for this project. 
+Generally, i wanted to increase my overall knowledge and familiarity with python. 
+More specifically, I had the following goals in mind: 
+
+- Use python to open a very lightweight database and use sqlalchemy as a method to query it. 
+I am very used to querying databases in raw sql so this was new to me. 
+
+- I wanted to build a small weight GUI and researched Tkinter a bit for a basic UI with some interactivity
+
+- Setting up a full-fletched project with a more typical python structure (seperate logic and UI, License, Readme) and use git to share and document it.
--- address-book/address_book/functions.py
+++ address-book/address_book/functions.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine, MetaData, select, insert
-from sqlalchemy.orm import sessionmaker
 import pandas as pd
-import tkinter as tk
-import os
+
 from constants import DATABASE_URL
+from random import randint
 
 
 def get_all_data(): 
+    """ Get all the data from the database to fill the table, returns a dataframe"""
 
     engine = create_engine( f"sqlite:///{DATABASE_URL}")
     connection = engine.connect()
@@ -28,36 +28,95 @@ def get_all_data():
 
 
 def insert_data(dict): 
+    """ Pass dictionary to insert to database """
 
     engine = create_engine( f"sqlite:///{DATABASE_URL}")
-    #Session = sessionmaker(bind=engine)
-    #session = Session()
-
     connection = engine.connect()
     metadata = MetaData()
     metadata.reflect(bind=engine)
     address = metadata.tables['address']
 
-    connection.execute(address.insert(), [ 
-        dict
-    ])
+    connection.execute(address.insert(), [dict])
     connection.commit()
     connection.close()
 
 def selected_row(table): 
+    """ Get information from the selected row in the table and return as a dictionary"""
+    
     global selected_id
 
     selected = table.focus()
     details = table.item(selected)
     values = details.get("values")
-    print(details)
-    print(values)
     columns = list(table['columns'])
-    print(columns)
     id = details.get('text')
     selected_id = id
 
     dict_selected = dict(zip(columns, values))
     dict_selected.update({'id' : id})
-    print(dict_selected)
     return dict_selected
+
+
+def insert_dict(dict): 
+    """ Takes a dictionary and inserts it into the database"""
+    
+    engine = create_engine( f"sqlite:///{DATABASE_URL}")
+    connection = engine.connect()
+    metadata = MetaData()
+    metadata.reflect(bind=engine)
+    address = metadata.tables['address']
+
+    connection.execute(address.insert(), [dict])
+    connection.commit()
+    connection.close()
+
+
+def delete_entry(table): 
+    """ Takes row from the table and deletes it in the database """
+
+    # Retrieve ID from table 
+    selected = table.focus()
+    details = table.item(selected)
+    id = details.get("text")
+
+    # Delete ID from database
+    engine = create_engine( f"sqlite:///{DATABASE_URL}")
+    connection = engine.connect()
+    metadata = MetaData()
+    metadata.reflect(bind=engine)
+    address = metadata.tables['address']
+    qry = address.delete().where(address.c.id == id)
+    connection.execute(qry)
+    connection.commit()
+    connection.close()
+
+
+def update_entry(dict, selected): 
+    """ Use ID to update entries """
+    
+    # Retrieve data from entry widget via entry_to_dict
+    id = selected
+
+    # Delete ID from database
+    engine = create_engine( f"sqlite:///{DATABASE_URL}")
+    connection = engine.connect()
+    metadata = MetaData()
+    metadata.reflect(bind=engine)
+    address = metadata.tables['address']
+    qry = address.update().where(address.c.id == id).values(dict)
+    connection.execute(qry)
+    connection.commit()
+    connection.close()
+
+
+def refresh_table(table): 
+    """ Refresh the data from the table """
+
+    table.delete(*table.get_children())
+    df = get_all_data()
+    table['columns'] = df.columns.values.tolist()
+    for i in df.columns.values.tolist():
+        table.column(i, width=50)
+        table.heading(i, text=i)
+    for index, row in df.iterrows():
+        table.insert("", 'end', text=index, values=list(row))

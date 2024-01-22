e04a0c03bef749bf64137b0c551e463ef5fea323
NVandeVoorde <nicolasvdvoorde@gmail.com>
2024-01-22 20:54:47 +0100

    collision paddle

--- pong-adapted/pong_adapted/main.py
+++ pong-adapted/pong_adapted/main.py
@@ -20,18 +20,35 @@ ball_x, ball_y = WIDTH / 2 - radius, HEIGHT / 2 - radius
 ball_vel_x, ball_vel_y = 0.25, 0.25
 
 # Paddle 
-paddle_width, paddle_height = 20, 120
+paddle_width, paddle_height = 20, 100
 left_paddle_y =  right_paddle_y = HEIGHT / 2 - paddle_height / 2
-left_paddle_x, right_paddle_x = 100 - paddle_width/2, WIDTH - 100 - paddle_width/2
+left_paddle_x, right_paddle_x = 100 - paddle_width / 2, WIDTH - 100 - paddle_width / 2
+right_paddle_vel = left_paddle_vel = 0 
 
 
-# mainloop 
+# Mainloop 
 
 while run: 
     wn.fill(BLACK)
     for i in pygame.event.get(): 
         if i.type == pygame.QUIT: 
             run = False
+        # Pressing keys
+        elif i.type == pygame.KEYDOWN: 
+            if i.key == pygame.K_UP: 
+                right_paddle_vel = -1
+                print("right up")
+            if i.key == pygame.K_DOWN: 
+                right_paddle_vel = 1
+                print("right down")
+            if i.key == pygame.K_w: 
+                left_paddle_vel = -1
+            if i.key == pygame.K_s: 
+                left_paddle_vel = 1
+        # Key release
+        if i.type == pygame.KEYUP: 
+            right_paddle_vel = 0
+            left_paddle_vel = 0
         
     # Wall bounces 
     if ball_y <= 0 + radius or ball_y >= HEIGHT - radius: 
@@ -44,10 +61,33 @@ while run:
         ball_x, ball_y = WIDTH / 2 - radius, HEIGHT / 2 - radius
         ball_vel_x, ball_vel_y = 0.25, 0.25
 
+    # Paddle collisions
+    if left_paddle_x <= ball_x <= left_paddle_x + paddle_width:
+        if left_paddle_y <= ball_y <= left_paddle_y + paddle_height: 
+            ball_x = left_paddle_x + paddle_width
+            ball_vel_x *= -1
+    if right_paddle_x <= ball_x <= right_paddle_x + paddle_width:
+        if right_paddle_y <= ball_y <= right_paddle_y + paddle_height: 
+            ball_x = right_paddle_x
+            ball_vel_x *= -1
 
     # Movements
     ball_x += ball_vel_x
     ball_y += ball_vel_y
+    left_paddle_y += left_paddle_vel
+    right_paddle_y += right_paddle_vel
+
+    # Paddle movement 
+    if right_paddle_y >= HEIGHT - paddle_height: 
+        right_paddle_y = HEIGHT - paddle_height
+    if right_paddle_y <= 0: 
+        right_paddle_y = 0
+    if left_paddle_y >= HEIGHT - paddle_height: 
+        left_paddle_y = HEIGHT - paddle_height
+    if left_paddle_y <= 0: 
+        left_paddle_y = 0
+
+
             
     # Elements
     pygame.draw.circle(wn, BLUE, (ball_x, ball_y), radius)
